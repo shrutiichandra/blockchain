a361e66aa7215e2fc7f2268cc5ce43cf1c2fe642
20188202010 <shruti.chandra@students.iiit.ac.in>
2020-02-17 16:19:08 +0530

    ui, tasks show

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -4,4 +4,29 @@ contract TodoList{
 	///keep track of number of task counts, state variable
 	uint public taskCount = 0;
 
+
+	struct Task{
+		uint id;
+		string content;
+		bool completed;
+	}
+
+
+	mapping(uint => Task) public tasks;
+
+	constructor() public{
+		///add a default task
+		createTask("hello dude!");
+	}
+
+	function createTask(string memory _content) public{
+		taskCount ++;
+		///putting tasks inside map
+		tasks[taskCount] = Task(taskCount, _content, false);
+
+		///broadcast an event that task was made
+
+	}
+
+
 }
